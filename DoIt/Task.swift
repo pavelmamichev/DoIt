c20522ad1c99b5a1cc30992a28048083455db353
Pavel Mamichev <pavel.mamichev@gmail.com>
2017-07-20 00:22:41 +0100

    added second view controller, added title and back button

---
+++ DoIt/Task.swift
@@ -0,0 +1,15 @@
+//
+//  Task.swift
+//  DoIt
+//
+//  Created by Pavel Mamichev on 20/07/2017.
+//  Copyright © 2017 Pavel Mamichev. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    var name = ""
+    var important = false
+    
+}
--- DoIt/ViewController.swift
+++ DoIt/ViewController.swift
@@ -11,10 +11,15 @@ import UIKit
 class ViewController: UIViewController, UITableViewDelegate,UITableViewDataSource {
     
     @IBOutlet weak var tableView: UITableView!
+    
+    var tasks : [Task] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        tasks = makeTasks()
         
         tableView.dataSource = self
         tableView.delegate = self
@@ -22,13 +27,40 @@ class ViewController: UIViewController, UITableViewDelegate,UITableViewDataSourc
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return tasks.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
-        cell.textLabel?.text = "hello"
+        let task = tasks[indexPath.row]
+        if task.important {
+            cell.textLabel?.text = " 🥊 \(task.name)"
+        } else {
+            cell.textLabel?.text = task.name
+        }
+
         return cell
+    }
+    
+    func makeTasks() -> [Task] {
+        let task1 = Task()
+        task1.name = "Walk the dog"
+        task1.important = false
+        
+        let task2 = Task()
+        task2.name = "Buy Cheese"
+        task2.important = true
+        
+        let task3 = Task()
+        task3.name = "Mow the lawn"
+        task3.important = true
+        
+        return [task1, task2, task3]
+    }
+    
+    @IBAction func plusTapped(_ sender: Any) {
+        performSegue(withIdentifier: "addSegue", sender: nil)
+        
     }
 
 
